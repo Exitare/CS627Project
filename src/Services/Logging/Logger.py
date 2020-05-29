634eadfe95ae77d5d0d4688bddf05885aff70a63
Exitare <business.exitare@icloud.com>
2020-05-29 15:13:41 -0700

    Added logger console output.

--- src/Services/Logging/Logger.py
+++ src/Services/Logging/Logger.py
@@ -1,5 +1,10 @@
 import logging
 from enum import Enum
 from Services.Configuration.Config import Config
+import sys
 
 logging.basicConfig(filename='example.log', level=logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+root.addHandler(handler)
