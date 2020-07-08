f5878fe7b05a79bdd26a846079f73e3d3abace86
Exitare <business.exitare@icloud.com>
2020-07-08 14:01:41 -0700

    Updated args parser.

--- src/Services/Configuration/Argument_Parser.py
+++ src/Services/Configuration/Argument_Parser.py
@@ -12,15 +12,17 @@ def handle_args():
 
     parser = argparse.ArgumentParser(description='Get the impact of tool features on it\'s runtime.',
                                      epilog='Accepts tsv and csv files')
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store', required=False,
-                        help="Enables the verbose mode. With active verbose mode additional information is shown in the console")
-    parser.add_argument('-p', '--percentage', dest='percentage', default=False, required=False,
-                        help="Enable the detection and evaluation of multiple versions of tools present in the raw folder")
-    parser.add_argument('-r', '--remove', dest='remove', action='store', required=False,
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', required=False,
+                        help="Enables the verbose mode. With active verbose mode additional information"
+                             "is shown in the console")
+    parser.add_argument('-mg', '--merge', dest='merge', required=False, action='store_true',
+                        help="Merges all available version of a tool together.")
+    parser.add_argument('-r', '--remove', dest='remove', action='store_true', required=False,
                         help="Activates the removal of data for further evaluation of data sets")
-    parser.add_argument('-m', '--memory', dest='memory', action='store', required=False,
-                        help="If set, the application will run in memory saving mode. Should only be used where memory is limited.")
-    parser.add_argument('-d', '--debug', dest='debug', action='store', required=False,
+    parser.add_argument('-m', '--memory', dest='memory', action='store_true', required=False,
+                        help="If set, the application will run in memory saving mode."
+                             "Should only be used where memory is limited.")
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', required=False,
                         help="If set, the tool will run in debug mode. You will get developer output. The performance"
                              "is most likely be not as fast as possible!")
     args = parser.parse_args()
@@ -31,7 +33,7 @@ def handle_args():
     if args.verbose:
         Config.VERBOSE = True
 
-    if args.percentage:
+    if args.merge:
         Config.MERGED_TOOL_EVALUATION = True
 
     if args.memory:
